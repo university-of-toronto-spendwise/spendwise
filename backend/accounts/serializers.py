5d24ebe0652c517c4a8721204720909212312998
redpinecube <tara.chakkithara@icloud.com>
2026-02-21 18:04:27 -0600

    create accounts app
    Signed-off-by: redpinecube <tara.chakkithara@icloud.com>

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model 
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'password2')
+        extra_kwargs = {
+            'email': {'required': True},
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
